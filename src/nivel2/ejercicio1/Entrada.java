734bd35b4aeba4588a4c1b81f662db607ffd0a71
Laura Gambarte <lauu.91@gmail.com>
2022-07-07 22:52:46 +0200

    Fixed boolean of S1.02

--- src/nivel2/ejercicio1/Entrada.java
+++ src/nivel2/ejercicio1/Entrada.java
@@ -25,22 +25,24 @@ public class Entrada {
  */
     public static int readInt(String message) {
         int input = 0;
-        boolean continuar;//default value is false
+        boolean continuar = false;
         do {
             try {
 
                 System.out.println(message);
                 input = sc.nextInt();
-                continuar = false;//when everything goes well then continuar set to false to break the loop, otherwise
-                //if something goes bad exception is thrown and we jump to catvh
+                continuar = true;
+                //when everything goes well will be stored in variable input
+                //if something goes bad exception is thrown before continuar is set to true and we jump to catch and then we ask for input
+                //until everything goes weell and continuar changes to true
 
             } catch (InputMismatchException e) {
                 System.out.println("Error de formato. Introduzca un valor correcto");
-                continuar = true;
+
             }
             sc.nextLine();
 
-        } while (continuar);//
+        } while (continuar == false);//
         return input;
 
 
@@ -48,100 +50,126 @@ public class Entrada {
 
     public static byte readByte(String message) {
         byte input = 0;
-        boolean continuar;
+        boolean continuar = false;
         do {
             try {
                 System.out.println(message);
                 input = sc.nextByte();
-                continuar = false;
+                continuar = true;
 
 
             } catch (InputMismatchException e) {
                 System.out.println("Error de formato. Introduzca un valor correcto");
-                continuar = true;
+
 
             }
-            sc.nextLine();
-        } while (continuar);
+            sc.nextLine();// para limpiar el buffer,lee nro entrada.nextInt pero no el enter.
+        } while (continuar == false);
         return input;
     }
 
     public static float readFloat(String message) {
         float input = 0f;
-        boolean continuar;
+        boolean continuar = false;
         do {
             try {
                 System.out.println(message);
                 input = sc.nextFloat();
-                continuar = false;
+                continuar = true;
             } catch (InputMismatchException e) {
                 System.out.println("Error de formato. Introduzca un valor correcto");
-                continuar = true;
+
             }
             sc.nextLine();
-        } while (continuar);
+        } while (continuar == false);
         return input;
     }
 
     public static double readDouble(String message) {
         double input = 0d;
-        boolean continuar;
+        boolean continuar = false;
         do {
             try {
                 System.out.println(message);
                 input = sc.nextDouble();
-                continuar = false;
+                continuar = true;
             } catch (InputMismatchException e) {
                 System.out.println("Error de formato. Introduzca un valor correcto");
-                continuar = true;
+
             }
             sc.nextLine();
-        } while (continuar);
+        } while (continuar == false);
         return input;
     }
 
-  public static String readString(String message) {
-        String mensajeUser="";
-        boolean continuar;
+    public static String readString(String message) {
+        String mensajeUser = "";
+        boolean continuar = false;
 
 
         do {
             try {
                 System.out.println(message);
 
-                 mensajeUser=sc.nextLine();
-                 continuar=false;
+                mensajeUser = sc.nextLine();
+                continuar = true;
 
 
             } catch (Exception e) {
                 System.out.println("Error de formato. Introduzca un valor correcto");
-                continuar=true;
+
             }
-        } while (continuar);
-      return mensajeUser;
+        } while (continuar == false);
+        return mensajeUser;
     }
 
-       public static char readChar(String message) {
+    public static char readChar(String message) {
         String userMessage = "";
-        boolean continuar;
+        boolean continuar = false;
 
         do {
             try {
                 System.out.println(message);
                 userMessage = sc.nextLine();
-                continuar=false;
-                if (userMessage.length() != 1) {
-                    throw new CharacterFormatException("Introdujiste un texto. " + userMessage + " No un caracter.Vuelve a intentarlo.");
+
+                if (userMessage.length() == 1) {
+                    continuar = true;
+                } else {
+                    throw new CharacterFormatException("Introdujiste un texto. No un caracter.Vuelve a intentarlo.");
+                    //new object of type characterformatexception so can take any message.
                 }
 
             } catch (Exception e) {
                 System.out.println(e.getMessage());
-                continuar=true;
+
             }
+        } while (continuar == false);
+        return userMessage.charAt(0);
+    }
+
+    public static boolean readSiNo(String message) {
+        String userMessage = "";
+        boolean continuar = false;
+        boolean result = false;
 
 
-
-        }while (continuar);
-           return userMessage.charAt(0);
+        do {
+            try {
+                System.out.println(message);
+                userMessage = sc.nextLine();
+                if (userMessage.charAt(0) == 's') {
+                    result = true;
+                    continuar = true;
+                } else if (userMessage.charAt(0) == 'n') {
+                    result = false;
+                    continuar = true;
+                } else {
+                    throw new CharacterFormatException("Error de formato. Introduce si o no.");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while (continuar == false);
+return result;
     }
 }
--- src/nivel2/ejercicio1/Main.java
+++ src/nivel2/ejercicio1/Main.java
@@ -9,11 +9,12 @@ public class Main {
         int age = Entrada.readInt("Introduce tu edad:");
         //putting the return value from Entrada.readInt.[   () is a parameter to readInt function], into variable int age
         //return age from entrada return sc.NextInt()
-        byte computers = Entrada.readByte("Introduce el nro de computadoras que tienes en casa:");
-        float altura = Entrada.readFloat("Introduce tu  altura:");
+       byte computers = Entrada.readByte("Introduce el nro de computadoras que tienes en casa:");
+         float altura = Entrada.readFloat("Introduce tu  altura:");
         double pi = Entrada.readDouble("Introduce el numero Pi ");
         String message = Entrada.readString("Introduce un mensaje a leer:");
         char character = Entrada.readChar("Introduce un character:");
+        boolean siNo = Entrada.readSiNo("Quieres terminar:");
 
     }
 }
